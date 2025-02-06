a25c4e54eecf00ffc96738c8d9b7cf27d4fce432
prnvyellayi <prnvshanmukh00@gmail.com>
2025-02-06 15:39:53 +0530

    chore: banner text truncate

--- app/banner.tsx
+++ app/banner.tsx
@@ -20,9 +20,9 @@ export default function Banner({
     return (
         <div className="absolute bottom-0 z-20 left-0 w-full p-4 flex justify-center items-center">
             <div className="right-0 grow max-w-xs bg-white p-4 shadow-lg flex justify-between rounded-xl items-center">
-                <div className="flex flex-col gap-0 items-start justify-center">
-                    <span className="text-lg font-bold">{title}</span>
-                    <span className="text-gray-500 text-sm">{sub_title}</span>
+                <div className="flex flex-col gap-0 items-start justify-center w-4/5">
+                    <span className="text-lg font-bold truncate w-full">{title}</span>
+                    <span className="text-gray-500 text-sm truncate w-full">{sub_title}</span>
                 </div>
                 <Link href={redirect_url}>
                     <button className="bg-[#CCFF00] p-2 rounded-full">
