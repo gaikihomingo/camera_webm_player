1178790fb6beea2b4347c2082fb5adbc3dfb8e9d
prnvyellayi <prnvshanmukh00@gmail.com>
2025-02-06 15:21:18 +0530

    chore: minor ui

--- app/card-preview.tsx
+++ app/card-preview.tsx
@@ -146,7 +146,7 @@ export default function CardPreview({ videoUrl }: { videoUrl: string }) {
     ]);
 
     return (
-        <div className="relative w-fit mx-auto">
+        <div className="w-fit mx-auto h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <video
                 autoPlay 
                 muted 
